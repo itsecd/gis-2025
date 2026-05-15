a0280c4fc83e8370ad79a452e58d5f004276897c
Chuck-man <sgold2398@gmail.com>
2026-05-15 15:15:52 -0400

    Corrections

--- client/vite-project/src/main.js
+++ client/vite-project/src/main.js
@@ -1,43 +1,91 @@
+import { fromLonLat } from 'ol/proj';
 import 'ol/ol.css';
 
 import Map from 'ol/Map';
 import View from 'ol/View';
+
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+
+import ImageLayer from 'ol/layer/Image';
+import ImageWMS from 'ol/source/ImageWMS';
+
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
+
 import { stylefunction } from 'ol-mapbox-style';
 
-const vectorSource = new VectorSource({
+const overtureSource = new VectorSource({
   url: '/overture.geojson',
   format: new GeoJSON()
 });
 
-const vectorLayer = new VectorLayer({
-  source: vectorSource
+const overtureLayer = new VectorLayer({
+  source: overtureSource
 });
 
 fetch('/style.json')
-  .then(r => r.json())
-  .then(style => {
-    stylefunction(vectorLayer, style, 'overture');
+  .then((r) => r.json())
+  .then((style) => {
+    stylefunction(overtureLayer, style, 'overture');
   });
 
 const map = new Map({
   target: 'map',
   layers: [
-    new TileLayer({ source: new OSM() }),
-    vectorLayer
+    new TileLayer({
+      source: new OSM()
+    }),
+
+    new ImageLayer({
+      source: new ImageWMS({
+        url: 'http://localhost:8080/geoserver/gis/wms',
+        params: {
+          LAYERS: 'gis:buildings',
+          TILED: true,
+          TRANSPARENT: true
+        },
+        serverType: 'geoserver'
+      })
+    }),
+
+    new ImageLayer({
+      source: new ImageWMS({
+        url: 'http://localhost:8080/geoserver/gis/wms',
+        params: {
+          LAYERS: 'gis:roads',
+          TILED: true,
+          TRANSPARENT: true
+        },
+        serverType: 'geoserver'
+      })
+    }),
+
+    new ImageLayer({
+      source: new ImageWMS({
+        url: 'http://localhost:8080/geoserver/gis/wms',
+        params: {
+          LAYERS: 'gis:pois',
+          TILED: true,
+          TRANSPARENT: true
+        },
+        serverType: 'geoserver'
+      })
+    }),
+
+    overtureLayer
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([50.45, 53.23]),
+    zoom: 14
   })
 });
 
-vectorSource.once('change', () => {
-  if (vectorSource.getState() === 'ready') {
-    map.getView().fit(vectorSource.getExtent(), { padding: [50, 50, 50, 50] });
+overtureSource.once('change', () => {
+  if (overtureSource.getState() === 'ready') {
+    map.getView().fit(overtureSource.getExtent(), {
+      padding: [50, 50, 50, 50]
+    });
   }
 });
