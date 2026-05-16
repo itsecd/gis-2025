2277457a33ee7f53b5233a35f5949497f9e0fb9b
d7anSeR <kyspanova@icloud.com>
2026-05-16 12:11:27 -0700

    добавлена 3 лаба

---
+++ lab3/lab3.sql
@@ -0,0 +1,97 @@
+INSTALL spatial;
+INSTALL httpfs;
+
+LOAD spatial;
+LOAD httpfs;
+
+CREATE TABLE osm_data AS
+SELECT * FROM ST_Read('../lab1/map.json')
+WHERE building IS NOT NULL;
+
+CREATE TABLE links AS
+WITH raw_data AS (
+    SELECT *
+    FROM 'https://stac.overturemaps.org/2026-04-15.0/buildings/building/collection.json'
+),
+raw_links AS (
+    SELECT unnest(links) AS link
+    FROM raw_data
+),
+links AS (
+    SELECT row_number() OVER () id, link.href
+    FROM raw_links
+    WHERE link.type = 'application/geo+json'
+),
+raw_bboxes AS (
+    SELECT unnest(extent.spatial.bbox) bbox
+    FROM raw_data
+),
+bboxes AS (
+    SELECT row_number() OVER () id, bbox[1] xmin, bbox[2] ymin, bbox[3] xmax, bbox[4] ymax
+    FROM raw_bboxes
+)
+SELECT href, xmin, ymin, xmax, ymax
+FROM links
+JOIN bboxes ON links.id = bboxes.id;
+
+SET VARIABLE item_url = (
+    SELECT DISTINCT
+        'https://stac.overturemaps.org/2026-04-15.0/buildings/building/' || links.href
+    FROM links
+    JOIN osm_data
+        ON ST_Xmin(geom) BETWEEN links.xmin AND links.xmax
+        AND ST_Ymin(geom) BETWEEN links.ymin AND links.ymax
+    LIMIT 1
+);
+
+SET VARIABLE s3_href = (
+    SELECT assets.aws.alternate.s3.href
+    FROM read_json(getvariable('item_url'))
+);
+
+CREATE TABLE smth_data AS
+WITH osm_data_geom_bbox AS (
+    SELECT ST_Extent_Agg(geom) geom
+    FROM osm_data
+),
+osm_data_bbox AS (
+    SELECT ST_Xmin(geom) - 0.01 AS xmin,
+           ST_Ymin(geom) - 0.01 AS ymin,
+           ST_Xmax(geom) + 0.01 AS xmax,
+           ST_Ymax(geom) + 0.01 AS ymax
+    FROM osm_data_geom_bbox
+)
+SELECT * EXCLUDE geometry, geometry
+FROM read_parquet(getvariable('s3_href')) data
+JOIN osm_data_bbox
+    ON ST_Xmin(geometry) BETWEEN osm_data_bbox.xmin AND osm_data_bbox.xmax
+    AND ST_Ymin(geometry) BETWEEN osm_data_bbox.ymin AND osm_data_bbox.ymax
+WHERE try(ST_IsValid(geometry)) = true;
+
+COPY (
+    SELECT json_object(
+        'type', 'FeatureCollection',
+        'features', json_group_array(
+            json_object(
+                'type', 'Feature',
+                'geometry', ST_AsGeoJSON(ST_SetCRS(geometry, 'EPSG:4326'))::JSON,
+                'properties', json_object('id', id, 'source_type', source_type)
+            )
+        )
+    )
+    FROM (
+        SELECT DISTINCT ON (temp.id)
+            temp.geometry,
+            temp.id,
+            CASE
+                WHEN osm.geom IS NOT NULL THEN 'my'
+                WHEN list_contains(list_transform(temp.sources, s -> s.dataset), 'OpenStreetMap') THEN 'osm'
+                ELSE 'ml'
+            END AS source_type
+        FROM smth_data temp
+        LEFT JOIN osm_data osm
+            ON try(ST_Intersects(osm.geom, ST_SetCRS(temp.geometry, 'EPSG:4326'))) = true
+    )
+)
+TO '../lab2/client/public/overture.json'
+WITH (FORMAT CSV, HEADER false, QUOTE '');
